57a781886436cd97262d2c887db5fbc9fa934545
aliefprimagani73@gmail.com <aliefprimagani73@gmail.com>
2025-09-28 00:41:19 +0700

    awal 4 terbaru

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,13 +1,23 @@
-const helpers = require('../helpers');
-const { CustomErrorAPI } = require('../helpers/CustomError');
+const helpers = require("../helpers");
+const { CustomErrorAPI } = require("../helpers/CustomError");
 
 module.exports = {
   errorMiddleware: async (error, req, res, next) => {
-    if (error instanceof CustomErrorAPI) {
-      return helpers.response(res, error.statusCode, error.message);
-    }
+    try {
+      if (error instanceof CustomErrorAPI) {
+        return helpers.response(res, error.statusCode, error.message);
+      }
 
-    console.error("Unexpected Error:", error);
-    return helpers.response(res, 500, `Internal Server Error, ${error.message}`);
+      console.error("🔥 Internal Error:", error);
+
+      return helpers.response(
+        res,
+        500,
+        `Internal Server Error, ${error.message}`
+      );
+    } catch (err) {
+      console.error("🔥 Error in errorMiddleware:", err);
+      return res.status(500).json({ status: 500, message: "Fatal Error" });
+    }
   },
 };
